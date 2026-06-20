1bfff885255f3ddc0e5f34f96885a83d9679d672
ardnew <andrew@ardnew.com>
2026-06-20 17:29:33 -0500

    cli: cleanup linter warnings
    Signed-off-by: ardnew <andrew@ardnew.com>

--- cli/geometry.go
+++ cli/geometry.go
@@ -14,9 +14,6 @@ type (
 )
 
 type (
-	point[T coordinate] struct{ x, y T }
-	rect[T coordinate]  struct{ min, max point[T] }
-
 	span[T coordinate]   struct{ min, max T }
 	bounds[T coordinate] struct{ x, y span[T] }
 )
@@ -28,13 +25,5 @@ func makeMaxBounds[T coordinate](x, y T) bounds[T] {
 	}
 }
 
-func withMaxBounds[T coordinate](x, y T) option[bounds[T]] {
-	return func(b *bounds[T]) {
-		b.x.max = min(b.x.max, x)
-		b.y.max = min(b.y.max, y)
-	}
-}
-func (p point[T]) String() string  { return fmt.Sprintf("{x:%d,y:%d}", p.x, p.y) }
-func (r rect[T]) String() string   { return fmt.Sprintf("{min:%s,max:%s}", r.min, r.max) }
 func (s span[T]) String() string   { return fmt.Sprintf("{min:%d,max:%d}", s.min, s.max) }
 func (b bounds[T]) String() string { return fmt.Sprintf("{x:%s,y:%s}", b.x, b.y) }
